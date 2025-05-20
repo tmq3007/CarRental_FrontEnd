bc66def9bf39965194a42b17af3207dcd1e4e133
tmq3007 <quantm3007@gmail.com>
2025-05-20 22:04:21 +0700

    Initial commit add rtk query redux

---
+++ rental-car/src/app/layout.tsx
@@ -0,0 +1,27 @@
+// app/layout.tsx
+import "@/app/globals.css"
+import { Inter } from "next/font/google"
+import { ReactNode } from "react"
+import { ThemeProvider } from "@/components/theme-provider"
+import { ReduxProvider } from "@/components/redux-provider" // <-- Import Provider
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+    title: "Car Rental Marketplace",
+    description: "Rent a car today or list your car for rent",
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} min-h-screen bg-background text-foreground`}>
+        <ReduxProvider>
+            <ThemeProvider attribute="class" defaultTheme="white" enableSystem>
+                {children}
+            </ThemeProvider>
+        </ReduxProvider>
+        </body>
+        </html>
+    )
+}
