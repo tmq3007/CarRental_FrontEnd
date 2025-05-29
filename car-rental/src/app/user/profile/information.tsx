f2fb5fda3e3ec6203dff20213f08d0f82835c6d3
Quan Truong <quantmhe186941@fpt.edu.vn>
2025-05-29 17:59:18 +0700

    show information

--- car-rental/src/app/user/profile/information.tsx
+++ car-rental/src/app/user/profile/information.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { Calendar, Upload } from "lucide-react"
+import {Upload } from "lucide-react"
 import {UserProfile} from "@/lib/services/user-api";
 
 
