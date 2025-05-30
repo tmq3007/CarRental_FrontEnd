f3cae26d32edfc591a087374202847234fb65871
Quan Truong <quantmhe186941@fpt.edu.vn>
2025-05-30 09:20:37 +0700

    show information

--- car-rental/src/app/layout.tsx
+++ car-rental/src/app/layout.tsx
@@ -1,3 +1,4 @@
+'use client'
 import "@/app/globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/provider/theme-provider"
@@ -7,30 +8,42 @@ import ScrollToTopButton from "@/components/common/scroll-button";
 import ChatToggleButton from "@/components/chat-bot/chatbox";
 import StoreProvider, {Props} from "@/components/provider/StoreProvider"; // <-- Import Provider
 import { Toaster } from "@/components/ui/sonner"
+import {usePathname} from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata = {
-    title: "Car Rental Marketplace",
-    description: "Rent a car today or list your car for rent",
-}
+
 
 export default function RootLayout({ children }: Props) {
+    const pathname = usePathname()
+
+    // Kiểm tra nếu là trang đăng nhập hoặc đăng ký
+    const isAuthPage = pathname?.startsWith("/signin") || pathname?.startsWith("/signup")
+
     return (
         <html lang="en" suppressHydrationWarning>
         <body className={`${inter.className} min-h-screen bg-background text-foreground`}>
         <StoreProvider>
             <ThemeProvider attribute="class" defaultTheme="white" enableSystem>
-                <main className="min-h-screen">
-                    <Header />
-                    <div className="pt-24 sm:pt-28 md:pt-32">
+                {isAuthPage ? (
+                    // Trang auth (không có Header/Footer)
+                    <main className="min-h-screen">
                         {children}
-                    </div>
-                    <Toaster />
-                    <Footer />
-                    <ScrollToTopButton />
-                    <ChatToggleButton />
-                </main>
+                        <Toaster />
+                    </main>
+                ) : (
+                    // Trang thường (có đầy đủ layout)
+                    <main className="min-h-screen">
+                        <Header />
+                        <div className="pt-24 sm:pt-28 md:pt-32">
+                            {children}
+                        </div>
+                        <Toaster />
+                        <Footer />
+                        <ScrollToTopButton />
+                        <ChatToggleButton />
+                    </main>
+                )}
             </ThemeProvider>
         </StoreProvider>
         </body>
