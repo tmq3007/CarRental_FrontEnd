a45f82af72e12d95e5a6d0f76e21901c2ca1fbb5
HaloicCa <hungnvhe187005@fpt.edu.vn>
2025-06-16 09:01:28 +0700

    siu

--- car-rental/src/app/user/edit-car/page.tsx
+++ car-rental/src/app/user/edit-car/page.tsx
@@ -1,5 +1,28 @@
-import EditCarDetails from "@/components/homepage/edit-car-details"
+"use client"
 
-export default function Page() {
-    return <EditCarDetails />
+import EditCarDetails from "@/components/homepage/edit-car-details"
+import { useParams } from "next/navigation"
+import { useSelector } from "react-redux"
+import { RootState } from "@/lib/store"
+
+export default function EditCarPage() {
+    const params = useParams()
+    const carIdFromParams = params?.carId as string
+
+
+    const carIdFromStore = useSelector((state: RootState) => state.car?.id)
+    const carId = carIdFromParams || carIdFromStore || ""
+
+    if (isNaN(Number(carId))) {
+        return (
+            <div className="min-h-screen bg-gray-50 p-4 flex items-center justify-center">
+                <div className="text-center">
+                    <h2 className="text-xl font-semibold text-gray-900 mb-2">Invalid Car ID</h2>
+                    <p className="text-gray-600">Please provide a valid car ID.</p>
+                </div>
+            </div>
+        )
+    }
+
+    return <EditCarDetails carId={carId}/>
 }
--- car-rental/src/lib/services/car-api.tsx
+++ car-rental/src/lib/services/car-api.tsx
@@ -62,6 +62,5 @@ export const carApi = createApi({
 
 export const {
     useGetCarsQuery,
-
 } = carApi
 
