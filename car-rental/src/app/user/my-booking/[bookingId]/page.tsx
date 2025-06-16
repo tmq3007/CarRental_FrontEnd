21368735ed6d225edccea2ed75632a0e685db294
Quan Truong <quantmhe186941@fpt.edu.vn>
2025-06-16 08:57:53 +0700

    commit 16/6

--- car-rental/src/app/user/my-booking/[bookingId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import BookingDetails from "@/components/booking/booking-details";
-
-export default function Booking({ params }: { params: { bookingId: string } }) {
-    return (
-        <main className="container mx-auto py-6">
-            <BookingDetails bookingId={String(params.bookingId)}/>
-        </main>
-    )
-}
